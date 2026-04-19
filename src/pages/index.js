973c5dbb23251d2f692728322ecc372bd458e300
wzhu2026 <xuerzaizai@xuerzaizai.serv00.net>
2026-04-19 11:28:28 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 import * as React from "react"
+import Layout from "../components/Layout"
 
 const IndexPage = () => {
   return (
-    <main style={{ padding: '2rem' }}>
-      <h1>Gatsby 构建成功</h1>
-      <p>当前时间: {new Date().toLocaleString()}</p>
-      <a href="/about">关于页面</a>
-    </main>
+    <Layout title="欢迎学习 Gatsby">
+      <p>这是一个极简的 Gatsby 学习项目。</p>
+      <p>当前时间：{new Date().toLocaleString()}</p>
+      <p>你可以通过修改这些文件来学习：</p>
+      <ul>
+        <li><code>src/pages/</code> - 页面文件</li>
+        <li><code>src/components/</code> - 组件文件</li>
+        <li><code>gatsby-config.js</code> - 配置文件</li>
+      </ul>
+    </Layout>
   )
 }
 
