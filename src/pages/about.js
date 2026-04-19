109d3d6fc61d55a06608a0628ae06ecff439d5af
wzhu2026 <xuerzaizai@xuerzaizai.serv00.net>
2026-04-19 11:10:10 +0800

    Create about.js

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const AboutPage = () => {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>关于</h1>
+      <p>这是关于页面</p>
+      <a href="/">返回首页</a>
+    </main>
+  )
+}
+
+export default AboutPage
