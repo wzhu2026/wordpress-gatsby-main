deeee9c56e4aa150ea3e7f3642a238dab2abe5e5
wzhu2026 <xuerzaizai@xuerzaizai.serv00.net>
2026-04-19 12:56:22 +0800

    Update post.js

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,28 +1,35 @@
 import * as React from "react"
 import Layout from "../components/Layout"
+import Seo from "../components/Seo"
 
 const PostTemplate = ({ pageContext }) => {
   const { post } = pageContext
 
   if (!post) {
     return (
-      <Layout title="文章未找到">
-        <p>抱歉，您访问的文章不存在。</p>
-        <a href="/">返回首页</a>
-      </Layout>
+      <>
+        <Seo title="文章未找到" />
+        <Layout title="文章未找到">
+          <p>抱歉，您访问的文章不存在。</p>
+          <a href="/">返回首页</a>
+        </Layout>
+      </>
     )
   }
 
   return (
-    <Layout title={post.title}>
-      <div style={{ marginBottom: '1rem', color: '#666' }}>
-        📅 {post.date} | 🏷️ {post.tags.join(', ')}
-      </div>
-      <div dangerouslySetInnerHTML={{ __html: post.content }} />
-      <div style={{ marginTop: '2rem', paddingTop: '1rem', borderTop: '1px solid #eee' }}>
-        <a href="/">← 返回首页</a>
-      </div>
-    </Layout>
+    <>
+      <Seo title={post.title} description={post.excerpt} article={true} />
+      <Layout title={post.title}>
+        <div style={{ marginBottom: '1rem', color: '#666' }}>
+          📅 {post.date} | 🏷️ {post.tags.join(', ')}
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: post.content }} />
+        <div style={{ marginTop: '2rem', paddingTop: '1rem', borderTop: '1px solid #eee' }}>
+          <a href="/">← 返回首页</a>
+        </div>
+      </Layout>
+    </>
   )
 }
 
