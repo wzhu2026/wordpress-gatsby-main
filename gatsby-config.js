6ccbe2925bd1cc2f8161364ea0f781c440e0111d
wzhu2026 <xuerzaizai@xuerzaizai.serv00.net>
2026-04-19 12:23:57 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: `我的博客`,
+    title: `我的学习项目`,
+    description: `学习 Gatsby 的示例项目`,
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
 }
