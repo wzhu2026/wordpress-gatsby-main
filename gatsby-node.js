dc0dbde77b1cbe10cfa2d348379d7b1068d0b8b2
wzhu2026 <xuerzaizai@xuerzaizai.serv00.net>
2026-04-19 12:04:57 +0800

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,18 @@
-// gatsby-node.js - 空文件，不做任何事情
-exports.sourceNodes = () => {}
-exports.createPages = () => {}
+const posts = require("./src/data/posts")
+
+exports.createPages = async ({ actions }) => {
+  const { createPage } = actions
+
+  // 为每篇文章创建详情页
+  posts.forEach(post => {
+    createPage({
+      path: `/post/${post.slug}`,
+      component: require.resolve("./src/templates/post.js"),
+      context: {
+        post: post
+      }
+    })
+  })
+
+  console.log(`✅ 创建了 ${posts.length} 个文章页面`)
+}
