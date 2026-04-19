c063d53e2d28f4852cbd767b3cc336bcb6379046
wzhu2026 <xuerzaizai@xuerzaizai.serv00.net>
2026-04-19 11:10:36 +0800

    Create 404.js

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+const NotFoundPage = () => {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>404 - 页面未找到</h1>
+      <a href="/">返回首页</a>
+    </main>
+  )
+}
+
+export default NotFoundPage
